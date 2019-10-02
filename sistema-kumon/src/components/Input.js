8dbcdff752b9716c62af8abb0632195f0aa1bc46
Eduardo Trujillo <edytr1997@gmail.com>
2019-10-02 14:15:43 -0500

    Handle user and password in login

--- sistema-kumon/src/components/Input.js
+++ sistema-kumon/src/components/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../App.css';
 
-function Input({type, name}) {
+function Input({type, name, changeHandler}) {
   return (
     <div className="TextInput">
       <label htmlFor={name}>{name}</label>
-      <input id={name} type={type}></input>
+      <input id={name} type={type} onChange={changeHandler}></input>
     </div>
   );
 }
--- sistema-kumon/src/components/TextInput.js
+++ sistema-kumon/src/components/TextInput.js
@@ -2,9 +2,9 @@ import React from 'react';
 import '../App.css';
 import Input from './Input'
 
-function TextInput({name}) {
+function TextInput({name, changeHandler}) {
   return (
-    < Input type="text" name={name} />
+    < Input type="text" name={name} changeHandler={changeHandler} />
   );
 }
 
