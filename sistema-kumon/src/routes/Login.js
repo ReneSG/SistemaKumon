39ae7df9053e2b4859ea2d4e96e517964d983e12
Huvok <h.garciaperez58@gmail.com>
2019-10-02 16:12:17 -0500

    login api call and formatting

--- sistema-kumon/src/routes/Login.js
+++ sistema-kumon/src/routes/Login.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import axios from "axios";
+import "../App.css";
 
-import TextInput from '../components/TextInput'
-import PasswordInput from '../components/PasswordInput';
+import TextInput from "../components/TextInput";
+import PasswordInput from "../components/PasswordInput";
+import { API_URL } from "../constants/apiurl";
+import { AUTHENTICATED } from "../constants/sessionstorage";
 
 class Login extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      "email": "",
-      "password": "",
-    }
+      email: "",
+      password: "",
+      isInvalid: false
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
@@ -20,12 +23,38 @@ class Login extends React.Component {
   handleChange(key, value) {
     this.setState({
       ...this.state,
-      [key]: value,
+      [key]: value
     });
   }
 
   handleLogin() {
     console.log(this.state);
+    var url = API_URL + "/auth/login";
+    const reqBody = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios
+      .post(url, reqBody)
+      .then(response => {
+        var data = response.data;
+        console.log(data);
+        if (data.error) {
+          this.setState({
+            isInvalid: true
+          });
+          /* TO-DO: ERROR HANDLING */
+        } else {
+          sessionStorage.setItem(AUTHENTICATED, true);
+          this.setState({
+            isInvalid: false
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
@@ -34,8 +63,18 @@ class Login extends React.Component {
         <header className="Login-header">
           <h1>Login</h1>
         </header>
-        <TextInput name="Correo" changeHandler={(event) => this.handleChange("email", event.target.value)}/>
-        <PasswordInput name="Contraseña" changeHandler={(event) => this.handleChange("password", event.target.value)} />
+        <TextInput
+          name="Correo"
+          changeHandler={event =>
+            this.handleChange("email", event.target.value)
+          }
+        />
+        <PasswordInput
+          name="Contraseña"
+          changeHandler={event =>
+            this.handleChange("password", event.target.value)
+          }
+        />
         <button onClick={this.handleLogin}>Login</button>
       </div>
     );
