9e2b7f2cac6a55a427ae6740e955efbc137fa807
Eduardo Trujillo <edytr1997@gmail.com>
2019-10-10 18:35:26 -0500

    Show payment date for student

--- sistema-kumon/src/routes/MarkAttendance.js
+++ sistema-kumon/src/routes/MarkAttendance.js
@@ -3,12 +3,20 @@ import "../App.css";
 
 import TextInput from "../components/TextInput";
 import { markAttendance } from "../controllers/StudentsController";
+import StudentRow from "../components/StudentRow";
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
 
 class MarkAttendance extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      identifier: ""
+      "identifier": "",
+      "name": null,
+      "paymentDue": null,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,23 +30,57 @@ class MarkAttendance extends React.Component {
   }
 
   async handleSubmit() {
-    let result = await markAttendance(this.state.identifier);
-    console.log(result);
+    let student = await markAttendance(this.state.identifier);
+    if (student) {
+      this.setState({
+        ...this.state,
+        "name": `${student.first_name} ${student.last_name_father} ${student.last_name_mother}`,
+        "paymentDue": student.next_payment_date,
+      });
+
+      await sleep(5000);
+
+      this.setState({
+        "identifier": "",
+        "name": null,
+        "paymentDue": null,
+      });
+
+    }
   }
 
   render() {
+    let display = null;
+    let title = "";
+    if (this.state.name !== null) {
+      title = "Bienvenido/a";
+      display = (
+        <StudentRow
+          identifier={this.state.identifier}
+          name={this.state.name}
+          paymentDue={this.state.paymentDue}
+        />
+      );
+    } else {
+      title = "Asistencia";
+      display = (
+        <div>
+          <TextInput
+            name="Matricula"
+            changeHandler={event =>
+              this.handleChange("identifier", event.target.value)
+            }
+          />
+          <button onClick={this.handleSubmit}>Marcar Asistencia</button>
+        </div>
+      );
+    }
     return (
       <div className="Mark-Attendance">
         <header className="Mark-Attendance-header">
-          <h1>Asistencia</h1>
+          <h1>{title}</h1>
         </header>
-        <TextInput
-          name="Matricula"
-          changeHandler={event =>
-            this.handleChange("identifier", event.target.value)
-          }
-        />
-        <button onClick={this.handleSubmit}>Marcar Asistencia</button>
+        {display}
       </div>
     );
   }
