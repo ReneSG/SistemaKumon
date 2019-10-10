fe36650a25d82db7b27c4fc2ffcc3ea15c9a2f79
Enrique Barragan <ebarragan1997@gmail.com>
2019-10-09 20:21:21 -0500

    Next payment date

--- sistema-kumon-api/config/routes.rb
+++ sistema-kumon-api/config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users,
-  path: '',
-  path_names: {
-    sign_in: 'login',
-    sign_out: 'logout'
-  },
-  controllers: {
-    sessions: 'sessions'
-  }
+              path: '',
+              path_names: {
+                sign_in: 'login',
+                sign_out: 'logout'
+              },
+              controllers: {
+                sessions: 'sessions'
+              }
   resources :students
   get 'student/:student_id/next_payment_date', to: 'students#next_payment_date'
   post '/student/mark_attendance', to: 'students#mark_attendance'
