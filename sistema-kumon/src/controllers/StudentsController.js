b601bbc5facc69ab1c1cfbf1186d791bde5d2dc9
Enrique Barragan <ebarragan1997@gmail.com>
2019-11-07 18:28:57 -0600

    fix headers issue

--- sistema-kumon/src/controllers/StudentsController.js
+++ sistema-kumon/src/controllers/StudentsController.js
@@ -37,7 +37,7 @@ export const registerStudent = async (
   };
 
   try {
-    const response = await axios.post(url, { headers }, reqBody);
+    const response = await axios.post(url, reqBody, { headers });
     return response.data;
   } catch (error) {
     console.log(error);
@@ -102,7 +102,7 @@ export const markAttendance = async identifier => {
     const headers = {
       Authorization: "Bearer " + sessionStorage.getItem(TOKEN)
     };
-    const response = await axios.post(url, { headers }, reqBody);
+    const response = await axios.post(url, reqBody, { headers });
     return response.data;
   } catch (error) {
     console.log(error);
