572a96316ef68552803d40be1130646eadcf2f45
Eduardo Trujillo <edytr1997@gmail.com>
2019-11-14 17:15:49 -0600

    Revert change to apiurl

--- sistema-kumon/src/constants/apiurl.js
+++ sistema-kumon/src/constants/apiurl.js
@@ -5,6 +5,4 @@ if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   API_URL = "https://intense-sierra-56544.herokuapp.com";
 }
 
-API_URL = "https://intense-sierra-56544.herokuapp.com";
-
 export { API_URL };
