7ef8471e88339fd66f24645466dbf3f374785a40
Rene Garcia <rene.saaenz@gmail.com>
2019-11-14 18:48:26 -0600

    Destroy on cascade

--- sistema-kumon-api/app/models/student.rb
+++ sistema-kumon-api/app/models/student.rb
@@ -3,15 +3,15 @@ require 'active_support/core_ext'
 class Student < ApplicationRecord
   after_create :set_next_payment_date
   enum gender: %i[male female]
-  has_many :guardians
-  has_many :payments
-  has_many :attendances
-  has_one :emergency_contact
+  has_many :guardians, dependent: :destroy
+  has_many :payments, dependent: :destroy
+  has_many :attendances, dependent: :destroy
+  has_one :emergency_contact, dependent: :destroy
   belongs_to :school
-  has_one :address
-  has_one :hear_about_interview
-  has_one :reason_to_join_interview
-  has_one :expected_results_interview
+  has_one :address, dependent: :destroy
+  has_one :hear_about_interview, dependent: :destroy
+  has_one :reason_to_join_interview, dependent: :destroy
+  has_one :expected_results_interview, dependent: :destroy
 
   accepts_nested_attributes_for :address, :emergency_contact, :guardians
 
