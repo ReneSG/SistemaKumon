1c461d1b1d06ee4fef9c42d8e57b3f9de11889d9
Rene Garcia <rene.saaenz@gmail.com>
2019-12-26 15:58:25 -0600

    Styles mailer

--- sistema-kumon-api/spec/mailers/previews/payment_mailer_preview.rb
+++ sistema-kumon-api/spec/mailers/previews/payment_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/payment_mailer
 class PaymentMailerPreview < ActionMailer::Preview
-
+  def payment_confirmation
+    PaymentMailer.payment_confirmation(Student.first)
+  end
 end
